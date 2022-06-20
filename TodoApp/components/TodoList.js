2f5169ea4fc88d28a8d01f3416c08c430aba6c6c
rlarldud1234 <rlarldud6482@naver.com>
2022-06-20 22:57:47 +0900

    ⚡️ :: TodoList

--- TodoApp/components/TodoList.js
+++ TodoApp/components/TodoList.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {FlatList, View, Text, StyleSheet} from 'react-native';
 import TodoItem from './TodoItem';
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, onToggle, onRemove}) => {
   return (
     <FlatList
       ItemSeparatorComponent={() => <View style={styles.separator} />}
       style={styles.list}
       data={todos}
       renderItem={({item}) => (
-        <TodoItem id={item.id} text={item.text} done={item.done} />
+        <TodoItem
+          id={item.id}
+          text={item.text}
+          done={item.done}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
       )}
       keyExtractor={item => item.id.toString()}
     />
