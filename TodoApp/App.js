5aaeb54da0f64f3687719d6e43fb4fe3e10955c6
rlarldud1234 <rlarldud6482@naver.com>
2022-06-20 16:56:53 +0900

    App.js에 ToDoList

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -6,10 +6,11 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
 import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Empty from './components/Empty';
 import AddTodo from './components/AddTodo';
 
 import {
@@ -19,6 +20,8 @@ import {
   Text,
   useColorScheme,
   View,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 
 import {
@@ -28,7 +31,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -57,12 +60,24 @@ const Section = ({children, title}): Node => {
 };
 
 const App: () => Node = () => {
+  const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead />
-        <Empty />
-        <AddTodo />
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding', android: undefined})}
+          style={styles.avoid}>
+          <DateHead />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          <AddTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -70,6 +85,10 @@ const App: () => Node = () => {
 
 const styles = StyleSheet.create({
   block: {
+    flex: 1,
+    backgroundColor: 'while',
+  },
+  avoid: {
     flex: 1,
   },
 });
