9044c50b030d05412cfc69dabc78bad813cdc901
rlarldud1234 <rlarldud6482@naver.com>
2022-06-24 10:30:10 +0900

    ⚡️ :: Appjs error

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -1,56 +1,18 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import DeatilScrenn from './screens/DetailScreen';
-import {View, Text, TouchableOpacity, Button} from 'react-native';
-import HeaderlessScreen from './screens/HeaderlessScreen';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import MainScreen from './screens/MainScreen';
+import DetailScreen from './screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
-
-const Drawer = createDrawerNavigator();
-
-const HomeScreen = ({navigation}) => {
-  return (
-    <View>
-      <Text>Home</Text>
-      <Button title="Drawer 열기" onPress={() => navigation.openDrawer()} />
-      <Button
-        title="Setting 열기"
-        onPress={() => navigation.navigate('Setting')}
-      />
-    </View>
-  );
-};
-
-const SettingScreen = ({navigation}) => {
-  return (
-    <View>
-      <Text>Setting</Text>
-      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
-    </View>
-  );
-};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Home"
-        drawerPossition="left"
-        backBehavior="history">
-        <Drawer.Screnn
-          name="Home"
-          component={HomeScreen}
-          options={{title: '홈'}}
-        />
-        <Drawer.Screen
-          name="Setting"
-          component={SettingScreen}
-          options={{title: '설정'}}
-        />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
