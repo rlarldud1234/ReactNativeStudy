0f910080df0e4c3f4ee0b6b1f35ed690adf40dc8
rlarldud1234 <rlarldud6482@naver.com>
2022-06-30 23:02:01 +0900

    ⚡️ :: Screen 파일 생성

---
+++ DayLog/screens/FeedScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const FeedScreen = () => {
+  return <View style={styles.block} />;
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default FeedScreen;
---
+++ DayLog/screens/WriteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const WriteScreen = () => {
+  return <View style={styles.block} />;
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default WriteScreen;
