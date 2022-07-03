149cfe68afd9acc4b92456a426d993ddff93bc1c
rlarldud1234 <rlarldud6482@naver.com>
2022-07-03 16:21:11 +0900

    ⚡️ :: WriteScreen

--- DayLog/screens/WriteScreen.js
+++ DayLog/screens/WriteScreen.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import WriteEditor from '../components/WriteEditor';
+import WriteHeader from '../components/WriteHeader';
+import LogContext from '../contexts/LogContext';
+import {useNavigation} from '@react-navigation/native';
 
 const WriteScreen = () => {
-  return <View style={styles.block} />;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const navigation = useNavigation();
+
+  const {onCreate} = useContext(LogContext);
+  const onSave = () => {
+    onCreate({
+      title,
+      body,
+      date: new Date().toISOString(),
+    });
+    navigation.pop();
+  };
+
+  return (
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader onSave={onSave} />
+        <WriteEditor
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  avoidingView: {
+    flex: 1,
+  },
 });
 
 export default WriteScreen;
