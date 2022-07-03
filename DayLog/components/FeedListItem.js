f2923d74bdd6bfdeb3d0c327ac2b9e36887999c4
rlarldud1234 <rlarldud6482@naver.com>
2022-07-03 16:19:17 +0900

    ⚡️ :: FeedListItem

---
+++ DayLog/components/FeedListItem.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+import {format, formatDistanceToNow} from 'date-fns';
+import {ko} from 'date-fns/locale';
+
+const formatDate = date => {
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000;
+
+  if (diff < 60) {
+    return '방금전';
+  }
+  if (diff < 60 * 60 * 24 * 3) {
+    return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+  }
+  return format(d, 'PPP EEE p', {locale: ko});
+};
+
+const truncate = text => {
+  const replaced = text.replace(/\n/g, ' ');
+  if (replaced.length <= 100) {
+    return replaced;
+  }
+  return replaced.slice(0, 10).concat('...');
+};
+
+const FeedListItem = ({log}) => {
+  const {title, body, date} = log;
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        styels.block,
+        Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
+      ]}
+      android_ripple={{color: '#ededed'}}>
+      <Text style={styels.date}>{formatDate(date)}</Text>
+      <Text style={styels.title}>{title}</Text>
+      <Text style={styels.body}>{truncate(body)}</Text>
+    </Pressable>
+  );
+};
+
+const styels = StyleSheet.create({
+  block: {
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+  date: {
+    fontSize: 12,
+    color: '#546e7a',
+    marginBottom: 8,
+  },
+  title: {
+    color: '#263238',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  body: {
+    color: '#37474f',
+    fontSize: 16,
+    lineHeight: 21,
+  },
+});
+
+export default FeedListItem;
