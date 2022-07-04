8e8b746d1996c930551e2f98992a97979e1eae2a
rlarldud1234 <rlarldud6482@naver.com>
2022-07-04 23:22:56 +0900

    ⚡️ :: WriteScreen 수정

--- DayLog/screens/WriteScreen.js
+++ DayLog/screens/WriteScreen.js
@@ -1,24 +1,55 @@
 import React, {useContext, useState} from 'react';
-import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import {Alert, StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 import LogContext from '../contexts/LogContext';
 import {useNavigation} from '@react-navigation/native';
 
-const WriteScreen = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+const WriteScreen = ({route}) => {
+  const log = route.params?.log;
+
+  const [title, setTitle] = useState(log.title ?? '');
+  const [body, setBody] = useState(log.body ?? '');
   const navigation = useNavigation();
 
-  const {onCreate} = useContext(LogContext);
+  const {onCreate, onModify, onRemove} = useContext(LogContext);
   const onSave = () => {
-    onCreate({
-      title,
-      body,
-      date: new Date().toISOString(),
-    });
+    if (log) {
+      onModify({
+        id: log.id,
+        date: log.date,
+        title,
+        body,
+      });
+    } else {
+      onCreate({
+        title,
+        body,
+        date: new Date().toISOString(),
+      });
+    }
     navigation.pop();
+  };
+  const onAskRemove = () => {
+    Alert.alert(
+      '삭제',
+      '정말로 삭제하시겠습니까?',
+      [
+        {text: '취소', style: 'cancel'},
+        {
+          text: '삭제',
+          onPress: () => {
+            onRemove(log.id);
+            navigation.pop();
+          },
+          style: 'destructive',
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
   };
 
   return (
@@ -26,7 +57,11 @@ const WriteScreen = () => {
       <KeyboardAvoidingView
         style={styles.avoidingView}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <WriteHeader onSave={onSave} />
+        <WriteHeader
+          onSave={onSave}
+          onAskRemove={onAskRemove}
+          isEditing={!!log}
+        />
         <WriteEditor
           title={title}
           body={body}
