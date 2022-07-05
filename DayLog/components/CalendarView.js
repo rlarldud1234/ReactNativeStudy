5bc2d46f21e2e1df23f9e25e81323a7acd22f066
rlarldud1234 <rlarldud6482@naver.com>
2022-07-05 15:06:09 +0900

    ⚡️ :: CalendarView

---
+++ DayLog/components/CalendarView.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Calendar} from 'react-native-calendars';
+import {StyleSheet} from 'react-native';
+
+const CalendarView = ({markedDates, selectedDate, onSelectDate}) => {
+  const markedSelectedDates = {
+    ...markedDates,
+    [selectedDate]: {
+      selected: true,
+      marked: markedDates[selectedDate]?.marked,
+    },
+  };
+
+  return (
+    <Calendar
+      style={styles.calendar}
+      markedDates={markedSelectedDates}
+      onPress={day => {
+        onSelectDate(day.dateString);
+      }}
+      theme={{
+        selectedDayBackgroundColor: '#009688',
+        arrowColor: '#009688',
+        dotColor: '#009688',
+        todayTextColor: '#009688',
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  calendar: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#e0e0e0',
+  },
+});
+
+export default CalendarView;
