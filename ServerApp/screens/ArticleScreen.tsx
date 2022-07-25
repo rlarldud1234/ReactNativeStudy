b977358058aa8a10d698ef71c2678a4d023e77cb
rlarldud1234 <rlarldud6482@naver.com>
2022-07-25 16:21:28 +0900

    ⚡️ :: ArticleScreen

---
+++ ServerApp/screens/ArticleScreen.tsx
@@ -0,0 +1,21 @@
+import {RouteProp, useRoute} from '@react-navigation/core';
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {RootStackParamList} from './types';
+
+type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
+
+const ArticleScreen = () => {
+  const {params} = useRoute<ArticleScreenRouteProp>();
+  return (
+    <View style={styles.block}>
+      <Text>{params.id}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default ArticleScreen;
