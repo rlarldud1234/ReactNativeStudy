a1d376a8883a0fd6dea2d5829983a3ca73c1ab5d
rlarldud1234 <rlarldud6482@naver.com>
2022-07-25 16:22:10 +0900

    ⚡️ :: screens types

---
+++ ServerApp/screens/types.ts
@@ -0,0 +1,28 @@
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {
+  CompositeNavigationProp,
+  NavigatorScreenParams,
+  RouteProp,
+} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+export type MainTabParamList = {
+  Articles: undefined;
+};
+export type MainTabNavigationScreenParams =
+  NavigatorScreenParams<MainTabParamList>;
+export type MainTabNavigationProp = CompositeNavigationProp<
+  RootStackNavigationProp,
+  BottomTabNavigationProp<MainTabParamList>
+>;
+export type MainTabRouteProp = RouteProp<RootStackParamList, 'MainTab'>;
+
+/* RootStack */
+export type RootStackParamList = {
+  MainTab: MainTabNavigationScreenParams;
+  Article: {
+    id: number;
+  };
+};
+export type RootStackNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
